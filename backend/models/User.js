7ed766bff3eeef6bb258f044d87efd0f7550db60
mostafa <mostafameerzad@gmail.com>
2024-12-08 21:11:55 +0430

    Update User model

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,10 +1,37 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
 const userSchema = new mongoose.Schema({
-    name: {type: String, required: true},
-    email: {type: String, required: true},
-    password: {type: String, required: true},
-})
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+    minLength: 4,
+    maxLength: 50,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    unique: true,
+    match: [
+      /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+      "Please provide a valid email address.",
+    ],
+  },
+  password: { type: String, required: true, minLength: 8 },
+});
 
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+userSchema.methods.comparePassword = async function (candidatePassword) {
+  return await bcrypt.compare(candidatePassword, this.password);
+};
 const User = mongoose.model("User", userSchema);
 export default User;

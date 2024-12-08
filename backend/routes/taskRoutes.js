7728008bea42245b7571f083c9651058903e4d2d
mostafa <mostafameerzad@gmail.com>
2024-12-08 19:30:17 +0430

    Implement put and delete Task routes

--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -23,4 +23,31 @@ router.post("/", async (req, res) => {
     }
 })
 
+router.put("/:id", async (req, res) => {
+    const id = req.params.id;
+    const updates = req.body;
+    try {
+        const updatedTask = await Task.findByIdAndUpdate(id, updates, {new: true})
+        if (!updatedTask) {
+            return res.status(404).json({message: "Task not found"});
+        }
+
+        return res.status(200).json(updatedTask);
+    } catch (err) {
+        res.status(400).json({message: err.message});
+    }
+})
+
+router.delete("/:id", async (req, res) => {
+    const id = req.params.id;
+    try{
+        const deletedTask = await Task.findByIdAndDelete(id)
+        if (!deletedTask) { return res.status(404).json({message: "Task not found"}); }
+
+        res.status(200).json({message:"Task deleted successfully"});
+    }
+    catch(err){
+        res.status(400).json({message: err.message});
+    }
+})
 export default router

acd28f45df911eadd2b03aeab3f3a3f034ac4b72
mostafa <mostafameerzad@gmail.com>
2024-12-13 15:59:51 +0430

    validate user registration input

---
+++ backend/middlewares/validation.js
@@ -0,0 +1,20 @@
+import { body } from "express-validator";
+
+export const validation = [
+  body("email")
+    .normalizeEmail()
+    .trim()
+    .isEmail()
+    .withMessage("Please provide a valid email"),
+  body("password")
+    .trim()
+    .isLength({ min: 8, max: 16 })
+    .withMessage("Password must be at least 8 characters"),
+  body("name")
+    .trim()
+    .escape()
+    .notEmpty()
+    .withMessage("Name is required")
+    .isAlphanumeric()
+    .withMessage("Name must contain only letters and numbers"),
+];

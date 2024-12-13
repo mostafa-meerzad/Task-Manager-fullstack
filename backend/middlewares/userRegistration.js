2325d7980ba1527fe1ee3c623ec9b7b8a4e186ba
mostafa <mostafameerzad@gmail.com>
2024-12-13 18:20:11 +0430

    Implement login route

--- backend/middlewares/userRegistration.js
+++ backend/middlewares/userRegistration.js
@@ -1,6 +1,6 @@
 import { body } from "express-validator";
 
-export const validation = [
+const userRegistration = [
   body("email")
     .normalizeEmail()
     .trim()
@@ -18,3 +18,17 @@ export const validation = [
     .isAlphanumeric()
     .withMessage("Name must contain only letters and numbers"),
 ];
+
+const userLogin = [
+  body("email")
+    .normalizeEmail()
+    .trim()
+    .isEmail()
+    .withMessage("Please provide a valid email"),
+  body("password")
+    .trim()
+    .isLength({ min: 8, max: 16 })
+    .withMessage("Password must be at least 8 characters"),
+];
+
+export { userLogin, userRegistration };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,11 +1,15 @@
 import express from "express";
 import User from "../models/User.js";
-import { validation } from "../middlewares/validation.js";
+import {
+  userLogin,
+  userRegistration,
+} from "../middlewares/userRegistration.js";
 import { validationResult } from "express-validator";
+import jwt from "jsonwebtoken";
 
 const router = express.Router();
 
-router.post("/register", validation, async (req, res) => {
+router.post("/register", userRegistration, async (req, res) => {
   try {
     const errors = validationResult(req);
 
@@ -31,4 +35,33 @@ router.post("/register", validation, async (req, res) => {
   }
 });
 
+router.post("/login", userLogin, async (req, res) => {
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) {
+      return res
+        .status(400)
+        .json({ errors: { msg: "Invalid email or password" } });
+    }
+
+    const isMatch = await user.comparePassword(req.body.password);
+    if (!isMatch) {
+      return res
+        .status(400)
+        .json({ errors: [{ msg: "Invalid email or password" }] });
+    }
+
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
+    res.status(200).json(token);
+  } catch (err) {
+    res.status(500).json({ errors: [{ msg: "Internal server error" }] });
+  }
+});
 export default router;

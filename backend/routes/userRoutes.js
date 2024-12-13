29b335a39cf842a644f2ac90650204baee8eb515
mostafa <mostafameerzad@gmail.com>
2024-12-13 16:05:38 +0430

    implement user registration route

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,32 @@
 import express from "express";
 import User from "../models/User.js";
+import { validation } from "../middlewares/validation.js";
+import { validationResult } from "express-validator";
 
 const router = express.Router();
 
-router.post("/", async (req, res) => {
-  const user = new User(req.body);
+router.post("/register", validation, async (req, res) => {
+  try {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password, name } = req.body;
+
+    let user = await User.findOne({ email: email });
+
+    if (user) {
+      return res.status(400).json({ errors: [{ msg: "user already exists" }] });
+    }
+    user = new User({ email, password, name });
+    await user.save();
+
+    res.status(201).json({ user });
+  } catch (err) {
+    res.status(500).json({ errors: [{ mag: "Internal server error" }] });
+  }
 });
 
 export default router;
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import "./startup/db.js";
 import taskRoutes from "./routes/taskRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 
 dotenv.config();
 
@@ -9,7 +10,7 @@ const app = express();
 
 app.use(express.json());
 app.use("/api/tasks", taskRoutes);
-
+app.use("/api/users", userRoutes);
 app.get("/", (req, res) => {
   res.send("Backend is running!");
 });

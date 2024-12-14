570a720704cf310edbc4d6d2b89893270a2d0698
mostafa <mostafameerzad@gmail.com>
2024-12-14 16:55:45 +0430

    Configure and install Radix UI

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import {StrictMode} from 'react'
+import {createRoot} from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import "@radix-ui/themes/styles.css";
+import {Theme} from "@radix-ui/themes";
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <StrictMode>
+        <Theme accentColor="blue" grayColor="slate">
+            <App/>
+        </Theme>
+    </StrictMode>
 )
+

bac1ef5871539d2e960f536ba269f525c21889f0
mostafa <mostafameerzad@gmail.com>
2024-12-23 21:48:28 +0430

    Frontend: Build login form and handle user authentication token

---
+++ frontend/src/components/auth/Login.jsx
@@ -0,0 +1,51 @@
+import { useForm } from "react-hook-form";
+
+const Login = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = async (data) => {
+    // data.preventDefault();
+    // console.log(data);
+      try {
+        // const response = await fetch("http://localhost:5000/api/users/login", {
+        const response = await fetch("/api/users/login", {
+            method: "POST",
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(data),
+        })
+
+          if (!response.ok) {
+              throw new Error("Invalid credentials");
+          }
+
+          const token = await response.json();
+          localStorage.setItem("token", token);
+          console.log("Logged in successfully!, token saved: ", token);
+      }
+      catch (error) {
+          console.error("Login failed, ", error.message);
+          alert(error.message);
+      }
+  };
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div className="flex justify-start items-center">
+        <label htmlFor={"email"}>email: </label>
+        <input type="text" id="email" {...register("email", {required:"Email is required"})} />
+        {errors.email && <span> {errors.email.message}</span>}
+      </div>
+      <div className="flex justify-start items-center">
+        <label htmlFor={"password"}>password: </label>
+        <input type="password" id="password" {...register("password", {required:"Password is required"})} />
+        {errors.password && <span> {errors.password.message}</span>}
+      </div>
+        <br/>
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+export default Login;
--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,4 +1,12 @@
+import Login from "../components/auth/Login.jsx";
+
 const LoginPage = () => {
-  return <div>LoginPage</div>;
+  return (
+    <div>
+      <h2>LoginPage</h2>
+      <br/>
+      <Login />
+    </div>
+  );
 };
 export default LoginPage;

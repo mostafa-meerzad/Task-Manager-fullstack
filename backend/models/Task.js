8267065e421ba2e77606908ab3b1f7e5ad8ba234
mostafa <mostafameerzad@gmail.com>
2024-12-25 18:41:20 +0430

    Backend: add user field to task model

--- backend/models/Task.js
+++ backend/models/Task.js
@@ -4,6 +4,9 @@ const taskSchema = new mongoose.Schema({
   title: { type: String, required: true },
   description: { type: String },
   completed: { type: Boolean, default: false },
+  user:{
+    type:mongoose.Schema.Types.ObjectId, ref: "User", required: true
+  }
 });
 
 const Task = mongoose.model("Task", taskSchema);

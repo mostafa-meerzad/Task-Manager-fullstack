3e93ab8b8dd0efab07f90fabccfbbbac00ca9a7b
mostafa <mostafameerzad@gmail.com>
2024-12-25 23:43:15 +0430

    Frontend: reformat TaskList and TaskPage

--- frontend/src/components/tasks/TaskList.jsx
+++ frontend/src/components/tasks/TaskList.jsx
@@ -27,26 +27,17 @@ const TaskList = () => {
   }, []);
 
   return (
-    <div className={"flex gap-6 flex-col"}>
-      <h2>
-        TaskPage <span>{tasks.length}</span>
-      </h2>
-
-      <ul className={"flex flex-col gap-8"}>
-        {tasks.map((task) => (
-          <li
-            key={task._id}
-            className={"flex justify-start items-center gap-3"}
-          >
-            <h3 className={"text-lg font-medium"}>{task.title}</h3>
-            <p>{task.description}</p>
-            <div
-              className={`inline-block w-3 h-3 rounded-full border shadow ${task.completed ? "bg-green-400" : "bg-red-400"}`}
-            ></div>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <ul className={"flex flex-col gap-8 "}>
+      {tasks.map((task) => (
+        <li key={task._id} className={"flex justify-start items-center gap-3"}>
+          <h3 className={"text-lg font-medium"}>{task.title}</h3>
+          <p>{task.description}</p>
+          <div
+            className={`inline-block w-3 h-3 rounded-full border shadow ${task.completed ? "bg-green-400" : "bg-red-400"}`}
+          ></div>
+        </li>
+      ))}
+    </ul>
   );
 };
 export default TaskList;
--- frontend/src/pages/TaskPage.jsx
+++ frontend/src/pages/TaskPage.jsx
@@ -1,10 +1,11 @@
 import TaskList from "../components/tasks/TaskList.jsx";
 
 const TaskPage = () => {
-return(
-    <div>
-        <TaskList/>
+  return (
+    <div className={"flex gap-6 flex-col"}>
+      <h2>TaskPage</h2>
+      <TaskList />
     </div>
-)
+  );
 };
 export default TaskPage;

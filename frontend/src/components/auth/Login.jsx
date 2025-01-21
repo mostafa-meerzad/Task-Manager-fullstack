54e3cef0aea424cadd0c300196d01a8b5d28e9f0
mostafa <mostafameerzad@gmail.com>
2025-01-21 20:50:21 +0430

    Frontend: Style Login and Register

--- frontend/src/components/auth/Login.jsx
+++ frontend/src/components/auth/Login.jsx
@@ -1,48 +1,54 @@
-import { useForm } from "react-hook-form";
+import {useForm} from "react-hook-form";
 
 const Login = () => {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm();
+    const {
+        register,
+        handleSubmit,
+        formState: {errors},
+    } = useForm();
 
-  const onSubmit = async (data) => {
-      try {
-        const response = await fetch("/api/users/login", {
-            method: "POST",
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify(data),
-        })
+    const onSubmit = async (data) => {
+        try {
+            const response = await fetch("/api/users/login", {
+                method: "POST",
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify(data),
+            })
 
-          if (!response.ok) {
-              throw new Error("Invalid credentials");
-          }
+            if (!response.ok) {
+                throw new Error("Invalid credentials");
+            }
 
-          const token = await response.json();
-          localStorage.setItem("token", token);
-          console.log("Logged in successfully!, token saved: ", token);
-      }
-      catch (error) {
-          console.error("Login failed, ", error.message);
-          alert(error.message);
-      }
-  };
-  return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <div className="flex justify-start items-center">
-        <label htmlFor={"email"}>email: </label>
-        <input type="text" id="email" {...register("email", {required:"Email is required"})} />
-        {errors.email && <span> {errors.email.message}</span>}
-      </div>
-      <div className="flex justify-start items-center">
-        <label htmlFor={"password"}>password: </label>
-        <input type="password" id="password" {...register("password", {required:"Password is required"})} />
-        {errors.password && <span> {errors.password.message}</span>}
-      </div>
-        <br/>
-      <button type="submit">Submit</button>
-    </form>
-  );
+            const token = await response.json();
+            localStorage.setItem("token", token);
+            console.log("Logged in successfully!, token saved: ", token);
+        } catch (error) {
+            console.error("Login failed, ", error.message);
+            alert(error.message);
+        }
+    };
+    return (
+        <form onSubmit={handleSubmit(onSubmit)}
+              className={"flex flex-col gap-4 justify-center items-start p-4 bg-[#2C2C38] text-white w-80  rounded-md"}>
+            <h3 className={"text-xl font-bold"}>Login</h3>
+            <div className="flex flex-col gap-2 w-full items-start">
+                <label htmlFor={"email"} className={"font-semibold"}>Email: </label>
+                <input type="text" id="email" {...register("email", {required: "Email is required"})}
+                       className={`w-full bg-inherit  border rounded p-1 ${errors.email ? "border-red-400" : "border-gray-500"}`}
+                       placeholder={"Your email address"}/>
+                {errors.email && <span className={"text-red-400 text-sm"}> {errors.email.message}</span>}
+            </div>
+            <div className="flex flex-col gap-2 w-full items-start">
+                <label htmlFor={"password"} className={"font-semibold"}>password: </label>
+                <input type="password" id="password" {...register("password", {required: "Password is required"})}
+                       className={`w-full bg-inherit  border rounded p-1 ${errors.password ? "border-red-400" : "border-gray-500"}`}
+                       placeholder={"Your password"}/>
+                {errors.password && <span className={"text-red-400 text-sm"}> {errors.password.message}</span>}
+            </div>
+            <br/>
+            <button type="submit" className={"bg-[#645FC6] w-full p-1.5 rounded-full font-bold m-auto -mt-4"}>Submit
+            </button>
+        </form>
+    );
 };
 export default Login;
--- frontend/src/components/auth/Register.jsx
+++ frontend/src/components/auth/Register.jsx
@@ -1,69 +1,78 @@
-import { useForm } from "react-hook-form";
+import {useForm} from "react-hook-form";
 
 const Register = () => {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm();
+    const {
+        register,
+        handleSubmit,
+        formState: {errors},
+    } = useForm();
 
-  const onSubmit = async (data) => {
-    try {
-      const response = await fetch("/api/users/register", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(data),
-      });
+    const onSubmit = async (data) => {
+        try {
+            const response = await fetch("/api/users/register", {
+                method: "POST",
+                headers: {"Content-Type": "application/json"},
+                body: JSON.stringify(data),
+            });
 
-      if (!response.ok) {
-        console.error("Invalid credentials");
-        alert("request failed");
-      }
-      // console.log(response.json())
-      const {user} = await response.json()
-      const token = user.token;
-      localStorage.setItem("token", token);
-      console.log("Logged in successfully!, token saved: ", token);
+            if (!response.ok) {
+                console.error("Invalid credentials");
+                alert("request failed");
+            }
+            // console.log(response.json())
+            const {user} = await response.json()
+            const token = user.token;
+            localStorage.setItem("token", token);
+            console.log("Logged in successfully!, token saved: ", token);
 
-      // const token = response.json()
-    } catch (err) {
-      console.log("register failed: ", err);
-      alert(err.message);
-    }
-  };
-  return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <div className="flex justify-start items-center">
-        <label htmlFor="name">name: </label>
-        <input
-          type="text"
-          id="name"
-          {...register("name", { required: "Name is required" })}
-        />
-        {errors.name && <span> {errors.name.message}</span>}
-      </div>
-      <div className="flex justify-start items-center">
-        <label htmlFor="email">email: </label>
-        <input
-          type="email"
-          id="email"
-          {...register("email", { required: "Name is required" })}
-        />
-        {errors.email && <span> {errors.email.message}</span>}
-      </div>
-      <div className="flex justify-start items-center">
-        <label htmlFor="password">password: </label>
-        <input
-          type="password"
-          id="password"
-          {...register("password", { required: "Name is required" })}
-        />
-        {errors.password && <span> {errors.password.message}</span>}
-      </div>
+            // const token = response.json()
+        } catch (err) {
+            console.log("register failed: ", err);
+            alert(err.message);
+        }
+    };
+    return (
+        <form onSubmit={handleSubmit(onSubmit)}
+              className={"flex flex-col gap-4 justify-center items-start p-4 bg-[#2C2C38] text-white w-80  rounded-md"}>
+            <h3 className={"text-xl font-bold"}>Register</h3>
+            <div className="flex flex-col gap-2 w-full items-start">
+                <label htmlFor="name" className={"font-semibold"}>name: </label>
+                <input
+                    type="text"
+                    id="name"
+                    {...register("name", {required: "Name is required"})}
+                    className={`w-full bg-inherit  border rounded p-1 ${errors.name ? "border-red-400": "border-gray-500" }`}
+                    placeholder={"Your name"}
+                />
+                {errors.name && <span className={"text-red-400 text-sm"}> {errors.name.message}</span>}
+            </div>
+            <div className="flex flex-col gap-2 w-full items-start">
+                <label htmlFor="email" className={"font-semibold"}>email: </label>
+                <input
+                    type="email"
+                    id="email"
+                    {...register("email", {required: "Name is required"})}
+                    className={`w-full bg-inherit  border rounded p-1 ${errors.email ? "border-red-400": "border-gray-500" }`}
+                    placeholder={"Your email address"}
+                />
+                {errors.email && <span className={"text-red-400 text-sm"}> {errors.email.message}</span>}
+            </div>
+            <div className="flex flex-col gap-2 w-full items-start">
+                <label htmlFor="password" className={"font-semibold"}>password: </label>
+                <input
+                    type="password"
+                    id="password"
+                    {...register("password", {required: "Name is required"})}
+                    className={`w-full bg-inherit  border rounded p-1 ${errors.password ? "border-red-400": "border-gray-500" }`}
+                    placeholder={"Your password"}
+                />
+                {errors.password && <span className={"text-red-400 text-sm"}> {errors.password.message}</span>}
+            </div>
 
-      <br />
-      <button type="submit">Submit</button>
-    </form>
-  );
+            <br/>
+            <button type="submit" className={"bg-[#645FC6] w-full p-1.5 rounded-full font-bold m-auto -mt-4"}>Submit
+            </button>
+        </form>
+    );
 };
 export default Register;

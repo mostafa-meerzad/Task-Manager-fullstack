ee4b9a443e582b07ca130e4310b34edcac59ddf1
mostafa <mostafameerzad@gmail.com>
2025-01-21 20:10:46 +0430

    Frontend: Style Layout

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -1,13 +1,13 @@
 import Navbar from "./navbar/Navbar.jsx";
 
-const Layout = ({ children }) => {
-  return (
-    <div>
-      <Navbar />
-      <main className={"flex m-3 p-4 gap-4 bg-gray-100 rounded-md"}>
-        {children}
-      </main>
-    </div>
-  );
+const Layout = ({children}) => {
+    return (
+        <div className={"flex"}>
+            <Navbar/>
+            <main className={" flex-1 p-4 bg-[#1E1E1E]"}>
+                {children}
+            </main>
+        </div>
+    );
 };
 export default Layout;
